6260faad032a3ebd22f23a5d329612e1dfef6cb7
BL4KCY <mostafafarssiui@gmail.com>
2024-06-30 22:11:53 +0100

    refactor: Update inputs.c for improved keyboard handling

--- src/inputs.c
+++ src/inputs.c
@@ -1,8 +1,23 @@
 #include "cub3d.h"
 
-int	keyboard(int keycode, t_info *mlx)
+int	keypress(int keycode, t_info *mlx)
 {
 	if (keycode == ESC)
 		ft_exit(mlx);
-	return (0);
+	if (keycode == UP)
+		mlx->player.walk_direction = 1;
+	if (keycode == DOWN)
+		mlx->player.walk_direction = -1;
+	if (keycode == LEFT)
+		mlx->player.turn_direction = -1;
+	if (keycode == RIGHT)
+		mlx->player.turn_direction = 1;
+}
+
+int	keyrelease(int keycode, t_info *mlx)
+{
+	if (keycode == UP || keycode == DOWN)
+		mlx->player.walk_direction = 0;
+	if (keycode == LEFT || keycode == RIGHT)
+		mlx->player.turn_direction = 0;
 }
