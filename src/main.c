27dea63d27b37b65e261378ce8c5c2bb4ffeb7e8
BL4KCY <mostafafarssiui@gmail.com>
2024-06-30 22:11:57 +0100

    refactor: Update main.c for improved code organization and readability

--- src/main.c
+++ src/main.c
@@ -6,22 +6,32 @@
 /*   By: melfersi <melfersi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 21:17:35 by melfersi          #+#    #+#             */
-/*   Updated: 2024/06/29 20:24:40 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/06/30 14:53:45 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void	*data(void)
+{
+	static t_info	mlx;
+	return (&mlx);
+}
+
 int	main(void)
 {
-	t_info	mlx;
+	t_info	*mlx;
 
-	mlx.mlx = mlx_init();
-	mlx.win = mlx_new_window(mlx.mlx, WIDTH, HEIGHT, "cub3d");
-	mlx.img = put_rec(mlx.mlx, WIDTH, HEIGHT, BCOLOR);
-	mlx_put_image_to_window(mlx.mlx, mlx.win, mlx.img, 0, 0);
-	mlx_hook(mlx.win, ON_DESTROY, KEYPRESSMASK, ft_exit, &mlx);
-	mlx_hook(mlx.win, ON_KEYDOWN, KEYPRESSMASK, keyboard, &mlx);
-	mlx_loop_hook(mlx.mlx, rendering, &mlx);
-	mlx_loop(mlx.mlx);
+	mlx = (t_info *)data();
+	mlx->mlx = mlx_init();
+	mlx->win = mlx_new_window(mlx->mlx, WIDTH, HEIGHT, "cub3d");
+	mlx->img = put_rec(mlx->mlx, WIDTH, HEIGHT, BCOLOR);
+	mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img, 0, 0);
+	mlx_hook(mlx->win, ON_DESTROY, KEYPRESSMASK, ft_exit, mlx);
+	mlx_hook(mlx->win, ON_KEYDOWN, KEYPRESSMASK, keypress, mlx);
+	mlx_hook(mlx->win, ON_KEYUP, KEYRELEASEMASK, keyrelease, mlx);
+	init_map(mlx);
+	init_player(mlx);
+	mlx_loop_hook(mlx->mlx, rendering, mlx);
+	mlx_loop(mlx->mlx);
 }
