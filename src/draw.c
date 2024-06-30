3205b6aef9e71a2f1af7258dd556b41b1fc29163
BL4KCY <mostafafarssiui@gmail.com>
2024-06-30 22:11:40 +0100

    feat: Add functions for rectangle drawing and line drawing

--- src/draw.c
+++ src/draw.c
@@ -7,7 +7,47 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
+void	rect(t_data *data, int x, int y, int height, int width, int color)
+{
+	for(int i = y; i < y + height; i++)
+	{
+		for(int j = x; j < x + width; j++)
+		{
+			if (i == y + height - 1 || j == x + width - 1)
+				my_mlx_pixel_put(data, j, i, 0x00000000);
+			else my_mlx_pixel_put(data, j, i, color);
+		}
+	}
+}
 
+void	draw_cir(t_data *data, int x, int y, int radius, int color)
+{
+	for (int i = y - radius; i < y + radius; i++)
+	{
+		for (int j = x - radius; j < x + radius; j++)
+		{
+			if (sqrt(pow(j - x, 2) + pow(i - y, 2)) <= radius)
+				my_mlx_pixel_put(data, j, i, color);
+		}
+	}
+}
+
+void	draw_line(t_data *data, int x1, int y1, int x2, int y2, int color)
+{
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+	int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
+	float Xinc = dx / (float)steps;
+	float Yinc = dy / (float)steps;
+	float X = x1;
+	float Y = y1;
+	for (int i = 0; i <= steps; i++)
+	{
+		my_mlx_pixel_put(data, X, Y, color);
+		X += Xinc;
+		Y += Yinc;
+	}
+}
 
 void	*put_rec(void *mlx, int w, int h, int color)
 {
