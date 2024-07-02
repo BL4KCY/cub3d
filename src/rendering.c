c9b2d89b20d44bd6fba2f29d608479ec3f9f758a
BL4KCY <mostafafarssiui@gmail.com>
2024-07-02 18:20:23 +0100

    refactor: Normalize player rotation angle in update_position function

--- src/rendering.c
+++ src/rendering.c
@@ -28,15 +28,23 @@ int	update_map(t_info *mlx)
 	return (0);
 }
 
+double	normalize_angle(double angle)
+{
+	angle = remainder(angle, (M_PI * 2));
+	if (angle < 0)
+		angle += (M_PI * 2);
+	return (angle);
+}
+
 void	update_position(t_info *mlx)
 {
 	double	new_x;
 	double	new_y;
 
 	mlx->player.rotation_angle += mlx->player.rotation_speed * mlx->player.turn_direction;
+	mlx->player.rotation_angle = normalize_angle(mlx->player.rotation_angle);
 	new_x = mlx->player.x + cos(mlx->player.rotation_angle) * mlx->player.move_speed * mlx->player.walk_direction;
 	new_y = mlx->player.y + sin(mlx->player.rotation_angle) * mlx->player.move_speed * mlx->player.walk_direction;
-	printf("new_x = %f, new_y = %f\n", new_x, new_y);
 	if (mlx->map.grid[(int)(new_y / TILE_SIZE)][(int)(new_x / TILE_SIZE)] == '0')
 	{
 		mlx->player.x = new_x;
@@ -44,17 +52,19 @@ void	update_position(t_info *mlx)
 	}
 }
 
-void	render_rays(t_info *mlx, double *ray)
+void	render_rays(t_info *mlx, t_ray *ray)
 {
 	int	i;
 	double	x1 = mlx->player.x;
 	double	y1 = mlx->player.y;
 
 	i = 0;
-	while (i < 1)
+	while (i < NUM_RAYS)
 	{
-		double	x2 = x1 + cos(ray[i]) * 300;
-		double	y2 = y1 + sin(ray[i]) * 300;
+		double	x2 = x1 + cos(ray[i].ray_ang) * mlx->player.ray[i].ray_dis;
+		double	y2 = y1 + sin(ray[i].ray_ang) * mlx->player.ray[i].ray_dis;
+		ray[i].is_ray_right ? puts("right"):puts("left");
+		ray[i].is_ray_up ? puts("up"):puts("down");
 		draw_line(&mlx->map.data, x1, y1, x2, y2, 0x00ff0000);
 		i++;
 	}
@@ -75,7 +85,7 @@ int	rendering(t_info *info)
 	update_map(info);
 	update_player(info);
 	raycasting(info);
-	render_rays(info, info->player.rays);
+	render_rays(info, info->player.ray);
 	mlx_put_image_to_window(info->mlx, info->win, info->map.data.img, 0, 0);
 	return (0);
 }
