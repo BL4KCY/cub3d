1181c87801fc26f67e2d08e35393cc2981ca1615
BL4KCY <mostafafarssiui@gmail.com>
2024-09-07 23:31:57 +0100

    finxing 42 norms

--- src/raycasting.c
+++ src/raycasting.c
@@ -3,14 +3,18 @@
 void	create_rays(t_info *info)
 {
 	double	ray_angle;
+	int		i;
 
 	ray_angle = info->player.rotation_angle - deg_to_rad(FOV_ANGLE / 2);
-	for (int i = 0; i < NUM_RAYS ; i++)
+	i = -1;
+	while (++i < NUM_RAYS)
 	{
 		info->player.ray[i].ray_ang = normalize_angle(ray_angle);
-		info->player.ray[i].is_ray_down = (info->player.ray[i].ray_ang > 0 && info->player.ray[i].ray_ang < M_PI);
+		info->player.ray[i].is_ray_down = (info->player.ray[i].ray_ang > 0
+				&& info->player.ray[i].ray_ang < M_PI);
 		info->player.ray[i].is_ray_up = !info->player.ray[i].is_ray_down;
-		info->player.ray[i].is_ray_right = (info->player.ray[i].ray_ang <= 0.5 * M_PI || info->player.ray[i].ray_ang > 1.5 * M_PI);
+		info->player.ray[i].is_ray_right = (info->player.ray[i].ray_ang <= 0.5
+				* M_PI || info->player.ray[i].ray_ang > 1.5 * M_PI);
 		info->player.ray[i].is_ray_left = !info->player.ray[i].is_ray_right;
 		ray_angle += deg_to_rad(FOV_ANGLE) / NUM_RAYS;
 	}
@@ -21,7 +25,7 @@ bool	hit_wall(t_info *info, double x, double y)
 	return (info->map.grid[(int)(y / T_SIZE)][(int)(x / T_SIZE)] == '1');
 }
 
-double	distence_ray(t_info *info, double x ,double y)
+double	distence_ray(t_info *info, double x, double y)
 {
 	if (info->player.x == x && info->player.y == y)
 		return (__DBL_MAX__);
@@ -33,11 +37,13 @@ void	raycasting(t_info *info)
 	t_intersec	intersec;
 	// double		dis_to_plane;
 	// double		correct_ray_dis;
+	int			i;
 
 	create_rays(info);
 	// dis_to_plane = (NUM_RAYS / 2) / tan(deg_to_rad(FOV_ANGLE / 2));
 	// info->player.plane_dis = dis_to_plane;
-	for (int i = 0; i < NUM_RAYS;i++)
+	i = -1;
+	while (++i < NUM_RAYS)
 	{
 		set_horizonal_intersection(info, &intersec, i);
 		set_vertical_intersection(info, &intersec, i);
