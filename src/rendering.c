ba247ee899e354cb8dca513f0fffa7920181df00
BL4KCY <mostafafarssiui@gmail.com>
2024-09-07 12:42:08 +0100

    Refactor rendering and player position update functions

--- src/rendering.c
+++ src/rendering.c
@@ -3,20 +3,27 @@
 int	update_minimap(t_info *info)
 {
 	int	color;
+	int	x;
+	int	y;
 
+	x = info->player.x <= 200 ? 0 : info->player.x - 200;
+	y = info->player.y <= 200 ? 0 : info->player.y - 200;
 	for (int i = 0; i < info->map.n_row; i++)
 	{
 		for (int j = 0; j < info->map.n_cols;j++)
 		{
 			if (info->map.grid[i][j] == '0')
-				color = 0x00ffffff;
+				color = WHITE;
 			else
-				color = 0x00000000;
-			rect(&info->map.data,
-			MINIMAP_SCALE_FAC * j * TILE_SIZE,
-			MINIMAP_SCALE_FAC * i * TILE_SIZE,
+				color = BROWN;
+			rect_cir(&info->map.data,
+			MINIMAP_SCALE_FAC * j * TILE_SIZE - MINIMAP_SCALE_FAC * x,
+			MINIMAP_SCALE_FAC * i * TILE_SIZE - MINIMAP_SCALE_FAC * y,
 			MINIMAP_SCALE_FAC * TILE_SIZE,
 			MINIMAP_SCALE_FAC * TILE_SIZE,
+			MINIMAP_SCALE_FAC * 200,
+			MINIMAP_SCALE_FAC * 200,
+			MINIMAP_SCALE_FAC * 200,
 			color);
 		}
 	}
@@ -38,8 +45,10 @@ void	update_Player_position(t_info *mlx)
 
 	mlx->player.rotation_angle += mlx->player.rotation_speed * mlx->player.turn_direction;
 	mlx->player.rotation_angle = normalize_angle(mlx->player.rotation_angle);
-	new_x = mlx->player.x + cos(mlx->player.rotation_angle) * mlx->player.move_speed * mlx->player.walk_direction;
-	new_y = mlx->player.y + sin(mlx->player.rotation_angle) * mlx->player.move_speed * mlx->player.walk_direction;
+	new_x = mlx->player.x + (cos(mlx->player.rotation_angle) * mlx->player.move_speed * mlx->player.walk_direction)
+						  + (mlx->player.move_rightleft * mlx->player.move_speed);
+	new_y = mlx->player.y + (sin(mlx->player.rotation_angle) * mlx->player.move_speed * mlx->player.walk_direction)
+						  + (mlx->player.move_updown * mlx->player.move_speed);
 	if (mlx->map.grid[(int)(new_y / TILE_SIZE)][(int)(new_x / TILE_SIZE)] == '0')
 	{
 		mlx->player.x = new_x;
@@ -55,8 +64,8 @@ void	render_rays(t_info *info)
 	while (i < NUM_RAYS)
 	{
 		draw_line(&info->map.data,
-		MINIMAP_SCALE_FAC * info->player.x,
-		MINIMAP_SCALE_FAC * info->player.y,
+		MINIMAP_SCALE_FAC * 200,
+		MINIMAP_SCALE_FAC * 200,
 		MINIMAP_SCALE_FAC * info->player.ray[i].hit_x,
 		MINIMAP_SCALE_FAC * info->player.ray[i].hit_y,
 		0x00ff0000);
@@ -67,15 +76,22 @@ void	render_rays(t_info *info)
 void	update_player(t_info *info)
 {
 	int	x2, y2;
+	int	limit_x, limit_y;
+	int	player_x, player_y;
 
+	player_x = (info->player.x < 200 ? info->player.x : 200);
+	player_y = (info->player.y < 200 ? info->player.y : 200);
 	draw_cir(&info->map.data,
-	MINIMAP_SCALE_FAC * info->player.x,
-	MINIMAP_SCALE_FAC * info->player.y,
+	MINIMAP_SCALE_FAC * player_x,
+	MINIMAP_SCALE_FAC * player_y,
 	MINIMAP_SCALE_FAC * info->player.radius,
-	0x00ff0000);
-	x2 = cos(info->player.rotation_angle) * 40 + info->player.x;
-	y2 = sin(info->player.rotation_angle) * 40 + info->player.y;
-	draw_line(&info->map.data, info->player.x * MINIMAP_SCALE_FAC, info->player.y * MINIMAP_SCALE_FAC, x2 * MINIMAP_SCALE_FAC, y2 * MINIMAP_SCALE_FAC, 0x00ff0000);
+	BLUE);
+	limit_x = 200;//info->player.x <= 200 ? 200 : info->player.x;
+	limit_y = 200;//info->player.y <= 200 ? 200 : info->player.y;
+	draw_empty_cir(&info->map.data, MINIMAP_SCALE_FAC * limit_x, MINIMAP_SCALE_FAC * limit_y, MINIMAP_SCALE_FAC * 200, BLUE);
+	x2 = cos(info->player.rotation_angle) * 40 + player_x;
+	y2 = sin(info->player.rotation_angle) * 40 + player_y;
+	draw_line(&info->map.data, player_x * MINIMAP_SCALE_FAC, player_y * MINIMAP_SCALE_FAC, x2 * MINIMAP_SCALE_FAC, y2 * MINIMAP_SCALE_FAC, BLUE);
 }
 
 
@@ -88,7 +104,7 @@ int	rendering(t_info *info)
 	// update_3d(info);
 	update_minimap(info);
 	update_player(info);
-	render_rays(info);
+	// render_rays(info);
 	mlx_put_image_to_window(info->mlx, info->win, info->map.data.img, 0, 0);
 	return (0);
 }
