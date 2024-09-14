041c14fff3b3a7ccab3542d4e384f597e8fe9daa
BL4KCY <mostafafarssiui@gmail.com>
2024-09-14 16:48:39 +0100

    Adjust player initialization: change radius to 8 and set rotation angle to WEST_P

--- src/logic/init.c
+++ src/logic/init.c
@@ -18,17 +18,15 @@ void	init_map(t_info *info, t_condition *condition)
 
 void	init_player(t_info *info, t_condition *condition)
 {
-	(void)condition;
-	printf("%d %d\n", condition->x_player, condition->y_player);
 	info->player.x = (condition->x_player * T_SIZE) + (T_SIZE / 2);
 	info->player.y = (condition->y_player * T_SIZE) + (T_SIZE / 2);
 	
-	info->player.radius = 10;
+	info->player.radius = 8;
 	info->player.turn_direction = 0;
 	info->player.walk_direction = 0;
 	info->player.move_updown = 0;
 	info->player.move_rightleft = 0;
-	info->player.rotation_angle = M_PI_2;
+	info->player.rotation_angle = WEST_P;
 	info->player.move_speed = 3;
 	info->player.rotation_speed = 2 * (M_PI / 180);
 	info->player.plane_dis = (info->width / 2) / tan(deg_to_rad(FOV_ANGLE / 2));
