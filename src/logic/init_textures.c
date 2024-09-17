3f7f4245c730c0433a2f4795ce2c6feb8cfaf606
BL4KCY <mostafafarssiui@gmail.com>
2024-09-17 19:17:33 +0100

    Add debug output for texture paths in init_texture function

--- src/logic/init_textures.c
+++ src/logic/init_textures.c
@@ -5,11 +5,12 @@ void	init_texture(t_info *info, t_condition *condition)
 {
 	int		i;
 
-	puts(ft_substr(condition->textures[0], 3, ft_strlen(condition->textures[0])));
 	info->tex.img[NORTH].path = condition->pure_texture[0];
 	info->tex.img[SOUTH].path = condition->pure_texture[1];
 	info->tex.img[WEST].path = condition->pure_texture[2];
 	info->tex.img[EAST].path = condition->pure_texture[3];
+	for (int i = 0; i < 4; i++)
+		printf("path: <%s>\n", info->tex.img[i].path);
 	i = -1;
 	while (++i < 4)
 	{
