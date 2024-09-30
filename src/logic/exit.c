64e2d7898c586b60616e705c92011c72e7440710
BL4KCY <mostafafarssiui@gmail.com>
2024-09-30 11:41:41 +0100

    Refactor exit function to improve resource cleanup and memory management

--- src/logic/exit.c
+++ src/logic/exit.c
@@ -1,8 +1,18 @@
 #include "cub3d.h"
 
-int	ft_exit(t_info *mlx)
+int	ft_exit(t_info *info)
 {
-	mlx_destroy_window(mlx->mlx, mlx->win);
-	free(mlx->mlx);
+	int		weapon_id;
+	int		i;
+
+	i = -1;
+	weapon_id = -1;
+	mlx_destroy_window(info->mlx, info->win);
+	mlx_destroy_image(info->mlx, info->map.data.img);
+	while (++i < 4)
+		mlx_destroy_image(info->mlx, info->tex.img[i].data.img);
+	mlx_destroy_display(info->mlx);
+	free(info->mlx);
+	ft_hunt_leak();
 	exit(0);
 }
