94dc6efe2790698c1732e01c75a38aa0223fabff
BL4KCY <mostafafarssiui@gmail.com>
2024-10-01 10:02:31 +0100

    Refactor texture offset calculations for improved clarity and maintainability

--- src/logic/rendering3d.c
+++ src/logic/rendering3d.c
@@ -6,7 +6,7 @@
 /*   By: melfersi <melfersi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 11:25:38 by melfersi          #+#    #+#             */
-/*   Updated: 2024/09/30 13:02:00 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/10/01 09:39:09 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,23 @@ int	check_direction(t_info *info, int x)
 	return (0);
 }
 
+int	offset_x(t_info *info, int direction, int x)
+{
+	if (info->player.ray[x].is_hor && info->player.ray[x].is_ray_up)
+		return (((int)info->player.ray[x].hit_x % T_SIZE)
+			* (info->tex.img[direction].width / T_SIZE));
+	if (info->player.ray[x].is_hor && info->player.ray[x].is_ray_down)
+		return ((T_SIZE - ((int)info->player.ray[x].hit_x % T_SIZE))
+			* (info->tex.img[direction].width / T_SIZE));
+	if (!info->player.ray[x].is_hor && info->player.ray[x].is_ray_left)
+		return ((T_SIZE - (int)info->player.ray[x].hit_y % T_SIZE)
+			* (info->tex.img[direction].width / T_SIZE));
+	if (!info->player.ray[x].is_hor && info->player.ray[x].is_ray_right)
+		return (((int)info->player.ray[x].hit_y % T_SIZE)
+			* (info->tex.img[direction].width / T_SIZE));
+	return (0);
+}
+
 void	set_wall_tex_pixel(t_info *info, int x, int y)
 {
 	int			dis_from_top;
@@ -46,11 +63,9 @@ void	set_wall_tex_pixel(t_info *info, int x, int y)
 		- (HEIGHT / 2 + 50);
 	info->tex.offset_y = dis_from_top
 		* (info->tex.img[direction].height / info->player.ray[x].strip_height);
-	info->tex.offset_x = ((int)info->player.ray[x].hit_y % T_SIZE)
-		* (info->tex.img[direction].width / T_SIZE);
-	if (info->player.ray[x].is_hor)
-		info->tex.offset_x = ((int)info->player.ray[x].hit_x % T_SIZE)
-			* (info->tex.img[direction].width / T_SIZE);
+	info->tex.offset_x = offset_x(info, direction, x);
+	if (direction == EAST_ID)
+		printf("offset_x: %d\n", info->tex.offset_x);
 	color = my_mlx_pixel_get(&info->tex.img[direction].data,
 			info->tex.offset_x, info->tex.offset_y);
 	alpha = (170 / info->player.ray[x].ray_dis);
